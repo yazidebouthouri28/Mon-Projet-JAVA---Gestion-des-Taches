ba1435e8ea1e333a9b4460667f9a729613651497
Yazide <ybouthouri740@gmail.com>
2025-04-30 13:54:30 +0100

    DATABASE FOUND TABLE NOT FOUND

--- Gui3B4/src/main/java/tn/esprit/utils/MyDataBase.java
+++ Gui3B4/src/main/java/tn/esprit/utils/MyDataBase.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class MyDataBase {
 
-    private final String URL = "jdbc:mysql://localhost:3306/gestion des lieux" ;
+    private final String URL = "jdbc:mysql://localhost:3306/gestion_des_lieux" ;
     private final String USER ="root";
     private final String PSW ="";
     private Connection connection;
